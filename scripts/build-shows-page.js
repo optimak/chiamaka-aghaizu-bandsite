12e48ab445437cc28eb3faec6d15076a0169b6cf
Chi Aghaizu <chiaghaizu@gmail.com>
2024-02-08 23:36:09 -0500

    add shows.css, addional assets and javascript files

---
+++ scripts/build-shows-page.js
@@ -0,0 +1,34 @@
+const showList = [
+    {
+        DATE: "Mon Sept 09 2024",
+        VENUE: "Ronald Lane",
+        LOCATION: "San Francisco, CA"
+    },
+    {
+        DATE: "Tue Sept 17 2024",
+        VENUE: "Pier 3 East",
+        LOCATION: "San Francisco, CA"
+    },
+    {
+        DATE: "Sat Oct 12 2024",
+        VENUE: "View Lounge",
+        LOCATION: "San Francisco, CA"
+    },
+    {
+        DATE: "Sat Nov 16 2024",
+        VENUE: "Hyatt Agency",
+        LOCATION: "San Francisco, CA"
+    }, 
+    {
+        DATE: "Fri Nov 29 2024",
+        VENUE: "Moscow Center",
+        LOCATION: "San Francisco, CA"
+    },
+    {
+        DATE: "Wed Dec 18 2024",
+        VENUE: "Press Club",
+        LOCATION: "San Francisco, CA"
+    },
+
+
+]

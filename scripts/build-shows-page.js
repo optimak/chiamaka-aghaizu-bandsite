9669c53c7ff5ca490b33df8eba30da7672e557c6
Chi Aghaizu <chiaghaizu@gmail.com>
2024-02-10 17:52:44 -0500

    add functional build-shows-page.js and updated styling for shows page

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -18,7 +18,7 @@ const showList = [
         DATE: "Sat Nov 16 2024",
         VENUE: "Hyatt Agency",
         LOCATION: "San Francisco, CA"
-    }, 
+    },
     {
         DATE: "Fri Nov 29 2024",
         VENUE: "Moscow Center",
@@ -30,5 +30,128 @@ const showList = [
         LOCATION: "San Francisco, CA"
     },
 
-
 ]
+
+
+const mainSection = document.querySelector(".main");
+// console.log(mainSection);
+// const showSection = document.querySelector(".main__shows-section");
+// console.log(showSection);
+const showSection = document.createElement("section");
+// console.log(showSection);
+showSection.classList.add("main__shows-section");
+
+
+const showTitle = document.createElement('h2');
+showTitle.innerText = 'Shows';
+showTitle.classList.add('main__shows-title');
+
+mainSection.appendChild(showSection);
+showSection.appendChild(showTitle);
+//
+for (let i = 0; i < showList.length; i++) {
+
+    const showStyler = document.createElement("div"); //FIRST
+    showStyler.classList.add("main__shows-styler");
+
+
+    // showStyler.addEventListener('click',)
+
+
+    const showDetailsStylerDate = document.createElement("div");
+    showDetailsStylerDate.classList.add("main__shows-details-styler");
+    const showDetailsStylerVenue = document.createElement("div");
+    showDetailsStylerVenue.classList.add("main__shows-details-styler");
+    const showDetailsStylerLocation = document.createElement("div");
+    showDetailsStylerLocation.classList.add("main__shows-details-styler");
+
+
+    const showButton = document.createElement("button");
+    showButton.innerText = 'BUY TICKETS';
+    showButton.classList.add("main__shows-button");
+
+    const showDivider = document.createElement("hr");
+    showDivider.classList.add("main__shows-divider");
+
+
+    const showSubtitleDate = document.createElement('h6');
+    showSubtitleDate.innerText = 'DATE';
+    showSubtitleDate.classList.add('main__shows-subtitle');
+
+    const showSubtitleVenue = document.createElement('h6');
+    showSubtitleVenue.innerText = 'VENUE';
+    showSubtitleVenue.classList.add('main__shows-subtitle');
+
+    const showSubtitleLocation = document.createElement('h6');
+    showSubtitleLocation.innerText = 'LOCATION';
+    showSubtitleLocation.classList.add('main__shows-subtitle');
+
+
+    const showTextDate = document.createElement('p');
+    showTextDate.innerText = showList[i].DATE;
+    showTextDate.classList.add('main__shows-details', 'main__shows-details--date');
+
+    const showTextVenue = document.createElement('p');
+    showTextVenue.innerText = showList[i].VENUE;
+    showTextVenue.classList.add('main__shows-details');
+
+    const showTextLocation = document.createElement('p');
+    showTextLocation.innerText = showList[i].LOCATION;
+    showTextLocation.classList.add('main__shows-details');
+
+    showSection.appendChild(showStyler); //first one
+    showStyler.appendChild(showDetailsStylerDate);
+    showStyler.appendChild(showDetailsStylerVenue);
+    showStyler.appendChild(showDetailsStylerLocation);
+    showStyler.appendChild(showButton);
+    showStyler.appendChild(showDivider);
+
+    showDetailsStylerDate.appendChild(showSubtitleDate);
+    showDetailsStylerDate.appendChild(showTextDate);
+    showDetailsStylerVenue.appendChild(showSubtitleVenue);
+    showDetailsStylerVenue.appendChild(showTextVenue);
+    showDetailsStylerLocation.appendChild(showSubtitleLocation);
+    showDetailsStylerLocation.appendChild(showTextLocation);
+}
+
+
+
+const shows = document.querySelectorAll('.main__shows-styler');
+console.log(shows);
+// Function to handle item click
+function selectOnClick(event) {
+    // console.log('EVENT init', event.currentTarget.classList[1])//value);
+    //Check if clicking on current selection
+    if (event.currentTarget.classList[1] == "main__shows-styler--selected") {
+        event.currentTarget.classList.remove('main__shows-styler--selected');
+        return
+    }
+ 
+      shows.forEach( (show) => show.classList.remove('main__shows-styler--selected'));
+    
+    // Add the 'selected' class to the clicked item
+    console.log('EVENT', event.currentTarget.classList[1])//value);
+    if (event.currentTarget.classList[0] == "main__shows-styler") {
+        event.currentTarget.classList.add('main__shows-styler--selected');
+    }
+    
+    
+}
+
+// shows.forEach(show => console.log(show));
+
+// Add click event listener to each item
+shows.forEach(show => {
+    show.addEventListener('click', selectOnClick);
+});
+
+
+
+// showStyler
+//
+
+
+
+
+// console.log(showSection);
+
